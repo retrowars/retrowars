4ecfdbb084d49d79e6dd677a259b527c259474a4
Peter Serwylo <peter@serwylo.com>
2021-06-23 12:00:21 +1000

    Load information about each webserver independantly.
    If any of them are unable to be reached, they are marked as 'inactive' and not shown to the user.
    If there are no active ones after completing the search, then a message is shown soliciting help
    in hosting servers.

--- core/src/com/serwylo/retrowars/net/OnlineServerDetails.kt
+++
@@ -1,3 +0,0 @@
-package com.serwylo.retrowars.net
-
-data class OnlineServerDetails(val id: String, val port: Int, val udpPort: Int)
--- core/src/com/serwylo/retrowars/net/ServerDirectory.kt
+++ core/src/com/serwylo/retrowars/net/ServerDirectory.kt
@@ -1,12 +1,19 @@
 package com.serwylo.retrowars.net
 
+import com.badlogic.gdx.Gdx
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
-import java.net.InetAddress
+import io.ktor.http.*
+import java.io.IOException
 
-data class ServerMetadataDTO(val hostname: InetAddress, val tcpPort: Int, val udpPort: Int, val type: String, val maxPlayersPerRoom: Int, val maxRooms: Int) {
+private const val TAG = "ServerDirectory"
+
+/**
+ * @param httpPort Used to fetch statistics about the server.
+ */
+data class ServerMetadataDTO(val hostname: String, val httpPort: Int) {
     companion object {
         const val SINGLE_ROOM = "singleRoom"
         const val PUBLIC_RANDOM_ROOMS = "publicRandomRooms"
@@ -14,14 +21,56 @@ data class ServerMetadataDTO(val hostname: InetAddress, val tcpPort: Int, val ud
     }
 }
 
+data class ServerInfoDTO(
+    val tcpPort: Int,
+    val udpPort: Int,
+    val type: String,
+    val maxPlayersPerRoom: Int,
+    val maxRooms: Int,
+    val currentRoomCount: Int,
+    val currentPlayerCount: Int,
+    val lastGameTimestamp: Long
+)
+
+/**
+ * @param info If this is null, it means that we have been unable to contact the server.
+ */
+data class ServerDetails(
+    val hostname: String,
+    val httpPort: Int,
+
+    val tcpPort: Int,
+    val udpPort: Int,
+    val type: String,
+    val maxPlayersPerRoom: Int,
+    val maxRooms: Int,
+    val currentRoomCount: Int,
+    val currentPlayerCount: Int,
+    val lastGameTimestamp: Long
+)
+
+private val httpClient = HttpClient(CIO) {
+    install(JsonFeature)
+}
+
 suspend fun fetchPublicServerList(): List<ServerMetadataDTO> {
     val url = "http://localhost:8080/.well-known/com.serwylo.retrowars-servers.json"
 
-    val client = HttpClient(CIO) {
-        install(JsonFeature)
+    return httpClient.get(url)
+}
+
+suspend fun fetchServerInfo(server: ServerMetadataDTO): ServerInfoDTO? {
+    val url = URLBuilder(
+        protocol = if (server.httpPort == 443) URLProtocol.HTTPS else URLProtocol.HTTP,
+        port = server.httpPort,
+        host = server.hostname,
+        encodedPath = "info"
+    ).build()
+
+    try {
+        return httpClient.get(url)
+    } catch (e: IOException) {
+        Gdx.app.error(TAG, "Could not fetch server metadata from $url", e)
+        return null
     }
-
-    val servers: List<ServerMetadataDTO> = client.get(url)
-
-    return servers
 }
