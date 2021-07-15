70df3519ab84bb8c396b1f3d03b8ff9d5db325ac
Peter Serwylo <peter@serwylo.com>
2021-07-15 17:09:59 +1000

    Allow specifying server port from command line.
    Helps with Heroku which really only runs on port 80 easily, whereas
    locally it makes more sense to use a non-privileged port such as 8080.
    
    Can use the PORT env variable, or the --port= CLI argument (with CLI
    taking precedence). If none is specified, defaults to 8080.

--- server/src/com/serwylo/retrowars/server/ServerLauncher.kt
+++ server/src/com/serwylo/retrowars/server/ServerLauncher.kt
@@ -2,13 +2,24 @@ package com.serwylo.retrowars.server
 
 import com.badlogic.gdx.backends.headless.HeadlessApplication
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration
+import com.serwylo.retrowars.net.Network
 
 object ServerLauncher {
 
     @JvmStatic
     fun main(arg: Array<String>) {
+
+        val envPort = System.getenv("PORT")?.toIntOrNull()
+
+        val cliPort = arg
+            .find { it.startsWith("--port=") }
+            ?.substring("--port=".length)
+            ?.toIntOrNull()
+
+        val port = cliPort ?: envPort ?: Network.defaultPort
+
         val config = HeadlessApplicationConfiguration()
-        HeadlessApplication(ServerApp(), config)
+        HeadlessApplication(ServerApp(port), config)
     }
 
 }
