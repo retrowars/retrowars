ae232f8451b7fe4b89609578e14965a44456d239
Peter Serwylo <peter@serwylo.com>
2021-08-01 15:54:06 +1000

    Tempest: Second level - a square with 4 segments on each size.

--- core/src/com/serwylo/retrowars/games/tempest/TempestGameState.kt
+++ core/src/com/serwylo/retrowars/games/tempest/TempestGameState.kt
@@ -3,7 +3,7 @@ package com.serwylo.retrowars.games.tempest
 import com.badlogic.gdx.math.Vector2
 
 class TempestGameState(private val worldWidth: Float, private val worldHeight: Float) {
-    val level: Level = makeFirstLevel(worldWidth, worldHeight)
+    val level: Level = makeSecondLevel(worldWidth, worldHeight)
 }
 
 data class Level(
@@ -34,6 +34,45 @@ private fun makeFirstLevel(worldWidth: Float, worldHeight: Float): Level {
 
     return Level(segments, true)
 
+}
+
+/**
+ * A square, with 4 segments on each size.
+ *
+ * https://youtu.be/jfaCrdBABUY?t=34
+ */
+private fun makeSecondLevel(worldWidth: Float, worldHeight: Float): Level {
+    val length = worldWidth.coerceAtMost(worldHeight) * 0.8f / 4
+    val center = Vector2(worldWidth / 2, worldHeight / 2)
+
+    val segments = listOf(
+        Segment(Vector2(-2 * length, -2 * length), Vector2(-length, -2 * length)),
+        Segment(Vector2(-length, -2 * length),        Vector2(0f, -2 * length)),
+        Segment(Vector2(0f, -2 * length),          Vector2(length, -2 * length)),
+        Segment(Vector2(length, -2 * length),         Vector2(2 * length, -2 * length)),
+
+        Segment(Vector2(2 * length, -2 * length), Vector2(2 * length, -length)),
+        Segment(Vector2(2 * length, -length),        Vector2(2 * length, 0f)),
+        Segment(Vector2(2 * length, 0f),          Vector2(2 * length, length)),
+        Segment(Vector2(2 * length, length),         Vector2(2 * length, 2 * length)),
+
+        Segment(Vector2(length, 2 * length),         Vector2(2 * length, 2 * length)),
+        Segment(Vector2(0f, 2 * length),          Vector2(length, 2 * length)),
+        Segment(Vector2(-length, 2 * length),        Vector2(0f, 2 * length)),
+        Segment(Vector2(-2 * length, 2 * length), Vector2(-length, 2 * length)),
+
+        Segment(Vector2(-2 * length, length),         Vector2(-2 * length, 2 * length)),
+        Segment(Vector2(-2 * length, 0f),          Vector2(-2 * length, length)),
+        Segment(Vector2(-2 * length, -length),        Vector2(-2 * length, 0f)),
+        Segment(Vector2(-2 * length, -2 * length), Vector2(-2 * length, -length)),
+    )
+
+    segments.forEach {
+        it.start.add(center)
+        it.end.add(center)
+    }
+
+    return Level(segments, true)
 }
 
 data class Segment(
