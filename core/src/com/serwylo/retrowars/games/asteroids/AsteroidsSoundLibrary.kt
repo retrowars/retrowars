2216810b9ac597d6de30fa763b2ac856cb35e34c
Peter Serwylo <peter@serwylo.com>
2022-05-11 21:50:53 +1000

    Added tick sound to space invaders.

--- core/src/com/serwylo/retrowars/games/asteroids/AsteroidsSoundLibrary.kt
+++ core/src/com/serwylo/retrowars/games/asteroids/AsteroidsSoundLibrary.kt
@@ -2,7 +2,17 @@ package com.serwylo.retrowars.games.asteroids
 
 import com.serwylo.retrowars.audio.SoundLibrary
 
-class AsteroidsSoundLibrary: SoundLibrary() {
+class AsteroidsSoundLibrary: SoundLibrary(
+    mapOf(
+    "asteroids_fire" to "asteroids_fire.ogg",
+    "asteroids_hit_asteroid_tiny" to "asteroids_hit_asteroid_tiny.ogg",
+    "asteroids_hit_asteroid_small" to "asteroids_hit_asteroid_small.ogg",
+    "asteroids_hit_asteroid_medium" to "asteroids_hit_asteroid_medium.ogg",
+    "asteroids_hit_asteroid_large" to "asteroids_hit_asteroid_large.ogg",
+    "asteroids_hit_ship" to "asteroids_hit_ship.ogg",
+    "asteroids_thrust" to "asteroids_thrust.ogg",
+    )
+) {
 
     fun fire() = play("asteroids_fire")
     fun hitShip() = play("asteroids_hit_ship")
