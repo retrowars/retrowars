6bed85135ac01050e9a99b8b5e4841d7584a5f03
Peter Serwylo <peter@serwylo.com>
2022-05-15 12:53:26 +1000

    Add remaining sounds to space invaders.
    The tick is a little oppressive, but at least we can play with it now.

--- core/src/com/serwylo/retrowars/games/spaceinvaders/SpaceInvadersSoundLibrary.kt
+++ core/src/com/serwylo/retrowars/games/spaceinvaders/SpaceInvadersSoundLibrary.kt
@@ -4,10 +4,20 @@ import com.serwylo.retrowars.audio.SoundLibrary
 
 class SpaceInvadersSoundLibrary: SoundLibrary(
     mapOf(
-    "spaceinvaders_tick" to "spaceinvaders_tick.ogg",
+        "spaceinvaders_tick" to "spaceinvaders_tick.ogg",
+        "spaceinvaders_alien_fire" to "spaceinvaders_alien_fire.ogg",
+        "spaceinvaders_ship_fire" to "spaceinvaders_ship_fire.ogg",
+        "spaceinvaders_hit_ship" to "spaceinvaders_hit_ship.ogg",
+        "spaceinvaders_hit_barrier" to "spaceinvaders_hit_barrier.ogg",
+        "spaceinvaders_hit_alien" to "spaceinvaders_hit_alien.ogg",
     )
 ) {
 
     fun tick() = play("spaceinvaders_tick")
+    fun alienFire() = play("spaceinvaders_alien_fire")
+    fun shipFire() = play("spaceinvaders_ship_fire")
+    fun hitShip() = play("spaceinvaders_hit_ship")
+    fun hitBarrier() = play("spaceinvaders_hit_barrier")
+    fun hitAlien() = play("spaceinvaders_hit_alien")
 
 }
