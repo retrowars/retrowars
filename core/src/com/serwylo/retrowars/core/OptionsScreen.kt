177691df529215e6b06f141cd7f6a16a7b550f86
Peter Serwylo <peter@serwylo.com>
2022-05-29 22:36:24 +1000

    Redesign options screen into columns to allow better placement of new options.
    This is in preparation for adding volume for sound + music.

--- core/src/com/serwylo/retrowars/core/OptionsScreen.kt
+++ core/src/com/serwylo/retrowars/core/OptionsScreen.kt
@@ -2,8 +2,11 @@ package com.serwylo.retrowars.core
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.ui.*
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.Align
 import com.serwylo.beatgame.ui.*
 import com.serwylo.beatgame.ui.UI_SPACE
@@ -20,100 +23,136 @@ import kotlin.random.Random
 
 class OptionsScreen(game: RetrowarsGame): Scene2dScreen(game, { game.showMainMenu() }) {
 
+    private val styles = game.uiAssets.getStyles()
+    private val skin = game.uiAssets.getSkin()
+    private val strings = game.uiAssets.getStrings()
+    private val sprites = game.uiAssets.getSprites()
+
     init {
         val stage = this.stage
-        val styles = game.uiAssets.getStyles()
-        val skin = game.uiAssets.getSkin()
-        val strings = game.uiAssets.getStrings()
-        val sprites = game.uiAssets.getSprites()
 
         val container = Table().apply {
+
             setFillParent(true)
             pad(UI_SPACE)
 
             row().top()
+
             add(
                 makeHeading(strings["options.title"], styles, strings) {
                     game.showMainMenu()
                 }
-            ).expandY()
+            ).colspan(2).expandY()
 
             row().pad(UI_SPACE)
+
             add(
-                CheckBox(strings["options.visual-effects"], skin).apply {
+                Table().also { wrapper ->
 
-                    isChecked = Options.useVisualEffects()
-
-                    addListener( object: ChangeListener() {
-                        override fun changed(event: ChangeEvent?, actor: Actor?) {
-                            Options.setUseVisualEffects(!Options.useVisualEffects())
-                        }
-                    })
+                    wrapper.row().pad(UI_SPACE)
+                    wrapper.add(Label(strings["options.controller-layouts"], styles.label.medium))
+                    wrapper.row().pad(UI_SPACE)
+                    wrapper.add(makeControllerSelectButtons())
+                    wrapper.row().pad(UI_SPACE)
+                    wrapper.add(makeChooseAvatarButton())
 
                 }
-            )
+            ).width(stage.viewport.worldWidth / 2f).pad(UI_SPACE).top()
 
-            row().padTop(UI_SPACE * 2).padBottom(UI_SPACE)
             add(
-                Label(strings["options.controller-layouts"], styles.label.medium)
-            )
+                Table().also { wrapper ->
+                    wrapper.add(makeVisualEffectsToggle()).growX()
+                }
+            ).width(stage.viewport.worldWidth / 2f).pad(UI_SPACE).top()
 
             row()
-            add(
-                HorizontalGroup().apply {
-                    Games.allAvailable
-                        .filter { it.controllerLayout != null }
-                        .forEach { gameDetails ->
-                            addActor(
-                                IconButton(skin, gameDetails.icon(sprites)) {
-                                    Gdx.app.postRunnable {
-                                        game.screen = ControllerSelectScreen(game, gameDetails)
-                                    }
-                                }
-                            )
-
-
-                    }
-                }
-            )
-
-            row().padTop(UI_SPACE * 2).padBottom(UI_SPACE)
-            add(
-                Label(strings["options.multiplayer-avatar"], styles.label.medium)
-            )
-
-            row()
-            val playerId = Options.getPlayerId()
-            val avatar = if (playerId == 0L) {
-                Label("?", game.uiAssets.getStyles().label.medium)
-            } else {
-                Avatar(playerId, game.uiAssets)
-                }
-
-            add(
-                Button(avatar, game.uiAssets.getSkin()).apply {
-                    addListener(object: ChangeListener() {
-                        override fun changed(event: ChangeEvent?, actor: Actor?) {
-                            Gdx.app.postRunnable {
-                                game.screen = AvatarSelectScreen(game)
-                            }
-                        }
-                    })
-                }
-            ).expandY().top()
+            add().expandY() // Used to centre the content above (as the heading also expandY()'s the top space)
 
         }
 
         stage.addActor(container)
 
+        stage.isDebugAll = true
+
         addToggleAudioButtonToMenuStage(game, stage)
+
+    }
+
+    private fun makeControllerSelectButtons() = VerticalGroup().also { col ->
+        Games.allAvailable
+                .filter { it.controllerLayout != null }
+                .chunked(3)
+                .forEach { games ->
+                    val row = HorizontalGroup()
+                    col.addActor(row)
+                    games.forEach { gameDetails ->
+                        row.addActor(
+                                IconButton(skin, gameDetails.icon(sprites)) {
+                                    Gdx.app.postRunnable {
+                                        game.screen = ControllerSelectScreen(game, gameDetails)
+                                    }
+                                }
+                        )
+                    }
+                }
+    }
+
+    private fun makeChooseAvatarButton(): Actor {
+
+        val action = {
+            Gdx.app.postRunnable {
+                game.screen = AvatarSelectScreen(game)
+            }
+        }
+
+        val playerId = Options.getPlayerId()
+        return if (playerId == 0L) {
+            makeButton("Choose Avatar", styles, action)
+        } else {
+            HorizontalGroup().also { row ->
+                row.addActor(Avatar(playerId, game.uiAssets))
+                row.addActor(makeButton("Change Avatar", styles, action))
+            }
+        }
+    }
+
+    /**
+     * This consists of a hack where I make my own checkbox (label + checkable box thing) because too much time was
+     * spent trying to get the text in the actual [CheckBox] to wrap properly.
+     */
+    private fun makeVisualEffectsToggle() = Table().also { wrapper ->
+
+        val checkbox = CheckBox("", skin).also { checkbox ->
+            checkbox.isChecked = Options.useVisualEffects()
+            checkbox.addListener( object: ChangeListener() {
+                override fun changed(event: ChangeEvent?, actor: Actor?) {
+                    val useEffects = !Options.useVisualEffects()
+                    Options.setUseVisualEffects(useEffects)
+                }
+            })
+        }
+
+        wrapper.add(checkbox).top()
+
+        wrapper.add(Label(strings["options.visual-effects"], styles.label.medium).also { label ->
+            label.touchable = Touchable.enabled
+            label.wrap = true
+            label.addListener( object: ClickListener() {
+                override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                    super.clicked(event, x, y)
+                    val useEffects = !Options.useVisualEffects()
+                    checkbox.isChecked = useEffects
+                    Options.setUseVisualEffects(useEffects)
+                }
+            })
+        }).growX()
 
     }
 
 }
 
 class AvatarSelectScreen(
-    game: RetrowarsGame,
+        game: RetrowarsGame,
 ): Scene2dScreen(game, { game.showOptions() }) {
 
     private var currentPlayerId = Options.getPlayerId()
@@ -232,18 +271,17 @@ class ControllerSelectScreen(
 
     init {
 
+        val styles = game.uiAssets.getStyles()
+        val strings = game.uiAssets.getStrings()
+        val sprites = game.uiAssets.getSprites()
+
         val container = Table().apply {
             setFillParent(true)
             pad(UI_SPACE)
         }
 
         container.add(
-            makeHeading(
-                gameDetails.icon(game.uiAssets.getSprites()),
-                game.uiAssets.getStrings()["options.controller-select.title"],
-                game.uiAssets.getStyles(),
-                game.uiAssets.getStrings()
-            ) {
+            makeHeading(gameDetails.icon(sprites), strings["options.controller-select.title"], styles, strings,) {
                 game.showOptions()
             }
         ).top().colspan(3)
