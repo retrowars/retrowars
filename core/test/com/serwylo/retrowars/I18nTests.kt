f1f5a10fc85f79f68a13cdc233c3edc129c7902c
Peter Serwylo <peter@serwylo.com>
2022-08-17 20:20:40 +1000

    Enable Estonian translation, add tests to check for malformed translations involving plurals.

---
+++ core/test/com/serwylo/retrowars/I18nTests.kt
@@ -0,0 +1,39 @@
+package com.serwylo.retrowars
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.utils.I18NBundle
+import de.tomgrill.gdxtesting.GdxTestRunner
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(GdxTestRunner::class)
+class I18nTests {
+
+    @Test
+    fun testStringFormats() {
+        UiAssets.supportedLocales.forEach { locale ->
+            val strings = I18NBundle.createBundle(Gdx.files.internal("i18n/messages"), Locale(locale))
+
+            val props = Properties().apply {
+                load(Gdx.files.internal("i18n/messages.properties").read())
+            }
+
+            val regex = Regex(".*\\{\\d+,choice,.*")
+            val toCheck = props.entries.filter {
+                it.value.toString().matches(regex)
+            }.map { it.key.toString() }
+
+            toCheck.forEach {
+                Gdx.app.log("I18nTests", "Locale: \"$locale\", String: \"$it\"")
+                val string = strings.format(it, 1)
+
+                assertNotNull(string)
+                assertNotEquals("", string)
+            }
+        }
+
+    }
+
+}
