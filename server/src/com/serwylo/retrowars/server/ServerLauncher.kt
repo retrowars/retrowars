650f2a6162b1d82783cb2132eb1ce2dbcde77bce
Peter Serwylo <peter@serwylo.com>
2024-01-07 08:36:22 +1100

    Add --help flag to server to show available args.

--- server/src/com/serwylo/retrowars/server/ServerLauncher.kt
+++ server/src/com/serwylo/retrowars/server/ServerLauncher.kt
@@ -5,17 +5,32 @@ import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration
 import com.serwylo.retrowars.net.Network
 import com.serwylo.retrowars.net.RetrowarsServer
 import com.serwylo.retrowars.utils.DesktopPlatform
+import kotlin.system.exitProcess
 
 object ServerLauncher {
+
+    val defaultMaxRooms = 20
+    val defaultRoomSize = 4
+    val defaultFinalScoreDurationMillis = 7500
+    val defaultInactivePlayerTimeout = 90000
 
     @JvmStatic
     fun main(args: Array<String>) {
 
+        val help = args.contains("--help") || args.contains("-h")
+        if (help) {
+            println("Usage: retrowars [--port=${Network.defaultPort}] [--max-rooms=${defaultMaxRooms}] [--room-size=${defaultRoomSize}] [--final-score-duration=${defaultFinalScoreDurationMillis}] [--inactive-player-timeout=${defaultInactivePlayerTimeout}] [--beta-games]\n" +
+                    "       retrowars --help\n" +
+                    "\n" +
+                    "Each argument can also be configured via a corresponding environment variable, e.g. PORT / MAX_ROOMS / ROOM_SIZE, etc.")
+            exitProcess(0)
+        }
+
         val port = getIntArg("PORT", "port", args) ?: Network.defaultPort
-        val maxRooms = getIntArg("MAX_ROOMS", "max-rooms", args) ?: 20
-        val roomSize = getIntArg("ROOM_SIZE", "room-size", args) ?: 4
-        val finalScoreDurationMillis = getIntArg("FINAL_SCORE_DURATION", "final-score-duration", args) ?: 7500
-        val inactivePlayerTimeoutMillis = getIntArg("INACTIVE_PLAYER_TIMEOUT", "inactive-player-timeout", args) ?: 90000
+        val maxRooms = getIntArg("MAX_ROOMS", "max-rooms", args) ?: defaultMaxRooms
+        val roomSize = getIntArg("ROOM_SIZE", "room-size", args) ?: defaultRoomSize
+        val finalScoreDurationMillis = getIntArg("FINAL_SCORE_DURATION", "final-score-duration", args) ?: defaultFinalScoreDurationMillis
+        val inactivePlayerTimeoutMillis = getIntArg("INACTIVE_PLAYER_TIMEOUT", "inactive-player-timeout", args) ?: defaultInactivePlayerTimeout
         val betaGames = getBoolArg("BETA_GAMES", "--beta-games", args)
 
         val serverConfig = RetrowarsServer.Config(
