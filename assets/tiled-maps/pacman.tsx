9a23bfcfa18c77b4de4111038601350b4f268f6b
Peter Serwylo <peter@serwylo.com>
2024-02-19 04:24:32 +1100

    WIP

---
+++ assets/tiled-maps/pacman.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pacman" tilewidth="8" tileheight="8" tilecount="31200" columns="240">
+ <image source="../retrowars-sprites/pacman-tiles.png" width="1920" height="1043"/>
+</tileset>
